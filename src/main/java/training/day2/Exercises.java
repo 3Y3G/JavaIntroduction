47a977369663913c99942395457bc238823c7d7d
3Y3G <radoslavdolarov@abv.bg>
2023-07-04 12:42:11 +0300

    Initial commit

--- src/main/java/training/day2/Exercises.java
+++ src/main/java/training/day2/Exercises.java
@@ -1,13 +1,10 @@
 package training.day2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Zadachi {
+public class Exercises {
     public static void main(String[] args) {
     }
 
