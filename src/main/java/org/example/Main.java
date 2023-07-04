c5af4e36e2eb5eac63a8b76f9ce625a0f3cb6e44
3Y3G <radoslavdolarov@abv.bg>
2023-07-04 09:31:23 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+        //String name = scanner.nextLine();
+        //int age = scanner.nextInt();
+
+        //System.out.println("Name: " + name);
+        //System.out.println("Age: " + age);
+        //System.out.println((char)21328);
+        Scanner in = new Scanner(System.in);
+        String day;
+        System.out.println("Enter value between 1 and 7: ");
+        int a = in.nextInt();
+        switch (a) {
+            case 1: day = "Monday";
+                break;
+            case 2: day = "Tuesday";
+                break;
+            case 3: day = "Wednesday";
+                break;
+            case 4: day = "Thursday";
+                break;
+            case 5: day = "Friday";
+                break;
+            case 6: day = "Saturday";
+                break;
+            case 7: day = "Sunday";
+                break;
+            default: day = "No such day!";
+                break;
+
+        }
+        System.out.println("The day is: " + day);
+    }
+}
