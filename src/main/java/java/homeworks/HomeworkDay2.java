b44a29fda89b4a882b4010871325e2760113ec59
3Y3G <radoslavdolarov@abv.bg>
2023-07-05 09:31:05 +0300

    Initial commit

--- src/main/java/java/homeworks/HomeworkDay2.java
+++ src/main/java/java/homeworks/HomeworkDay2.java
@@ -1,15 +1,10 @@
-package training.day2;
+package java.homeworks;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Homework {
-    public static void main(String[] args) {
-        homework2Ex3();
-    }
+public class HomeworkDay2 {
     static void homework2Ex3(){
         Scanner scanner = new Scanner(System.in);
 
@@ -54,32 +49,5 @@ public class Homework {
         for (String name : words) {
             System.out.println( name + " ");
         }
-    }
-    static void homework1(){
-        //HOMEWORK 03.07.2023
-        Scanner in = new Scanner(System.in);
-        String day;
-        System.out.println("Enter value between 1 and 7: ");
-        int a = in.nextInt();
-        switch (a) {
-            case 1: day = "Monday";
-                break;
-            case 2: day = "Tuesday";
-                break;
-            case 3: day = "Wednesday";
-                break;
-            case 4: day = "Thursday";
-                break;
-            case 5: day = "Friday";
-                break;
-            case 6: day = "Saturday";
-                break;
-            case 7: day = "Sunday";
-                break;
-            default: day = "No such day!";
-                break;
-
-        }
-        System.out.println("The day is: " + day);
     }
 }
