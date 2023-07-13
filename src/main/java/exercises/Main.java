7cb354207c5cce04bce94e267ff792a9aa015e4a
3Y3G <radoslavdolarov@abv.bg>
2023-07-13 10:17:20 +0300

    Initial commit

--- src/main/java/exercises/Main.java
+++ src/main/java/exercises/Main.java
@@ -1,14 +1,8 @@
 package exercises;
 
-
 import exercises.exercises.Calculator;
-import exercises.shapes.Rectangle;
-import exercises.shapes.Triangle;
-
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
